c5ffd0c0aa25f1f8e6b9c1f8aba198df8c5d8ece
Per Kristian Engebretsen <per.engebretsen@lindbak.no>
2023-05-18 21:23:21 +0200

    Setup articles cards

---
+++ src/components/svelte/CTA/index.svelte
@@ -0,0 +1,9 @@
+<script>
+  import hand from "/assets/hand.png"
+  export let url;
+  export let text;
+</script>
+
+<div class="mx-auto w-fit">
+  <a href={url} class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">{text}</a>
+</div>
