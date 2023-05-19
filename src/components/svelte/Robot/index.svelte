f421338a5b7ea3c02b1da51e863b9d331757b443
Per Kristian Engebretsen <per.engebretsen@lindbak.no>
2023-05-19 14:35:34 +0200

    Add lottie animation

---
+++ src/components/svelte/Robot/index.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount, afterUpdate } from 'svelte';
+  import lottie from 'lottie-web';
+  import animationData from './robot.json';
+
+  // Lottie
+  let animationContainer;
+  let animationInstance;
+
+  onMount(() => {
+    animationInstance = lottie.loadAnimation({
+      container: animationContainer,
+      animationData,
+    });
+  });
+</script>
+
+<div
+  bind:this={animationContainer}
+  class="h-[224px] object-contain mx-auto"
+/>
