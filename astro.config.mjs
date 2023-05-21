d38099c6fd2cc6a9ad7e25109207f2d4e9acb6fb
Per Kristian Engebretsen <per.engebretsen@lindbak.no>
2023-05-21 20:29:03 +0200

    Add astro sitemap and site integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
 
-import svelte from "@astrojs/svelte";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  site: 'https://hackitude.io',
+  integrations: [tailwind(), svelte(), sitemap()],
 });
